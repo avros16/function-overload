ece56b0760b1b2a4e6fed62072c9dfaedda0c180
avros16 <114757828+avros16@users.noreply.github.com>
2024-05-08 12:12:09 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@function-overloading/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std; 
+
+
+// in functions you can use the same name many times. this is called function overloading. for this, you need to change return type or number of parameters or type of parameters; 
+
+// the return type does not count as an overload (int num and void num) but this does -- >>
+
+// int add (int a, int b) and int add (float b, int a)
+void print_val(int size){
+
+  
+}
+
+
+void print_val(){
+
+  
+}
+
+
+
+
+
+
+
+
+int main() {
+  std::cout << "Hello World!\n";
+}
